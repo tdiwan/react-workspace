9336d359cdcf7753427dd0ae0bb6e70e8ba75d7d
Tushar Diwan <tushardiwan@Tushars-MacBook-Air.local>
2023-05-11 07:18:29 -0700

    next commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("h1",{
     id:"1",
     type:"xyz"
-},"Hello World from React");
+},"Hello World from React 🚀");
 const root =  ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading);
